978e3bbab9164b1531727aea13271d8bab4c272f
Isaac Tait <isaac@mountaintopcoding.com>
2024-02-04 15:21:08 -0500

    feat: update footer and readme 🏔️

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Footer() {
   return (
-    <div className='bg-slate-100 text-xs p-2'>
-      <p>Footer Items Here</p>
+    <div className='bg-slate-100 text-xs p-2 flex flex-row justify-between'>
+      <p>© 2006 - {new Date().getFullYear()}</p>
+      <Link href='https://github.com/Isaac-Tait/momsDemandActionConnecticut'>
+        <Image
+          src='/github-mark.png'
+          alt='github logo'
+          width={15}
+          height={15}
+        />
+      </Link>
+      <p>
+        A division of
+        <Link
+          href='https://www.everytown.org/'
+          className='hover:bg-blue-300 hover:text-red-500 p-1 text-blue-500'
+        >
+          Everytown Network
+        </Link>
+      </p>
     </div>
   );
 }
