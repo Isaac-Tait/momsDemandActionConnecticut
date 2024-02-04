ca1a86f701d81d46e4d0c280944c3799a8d635a0
Isaac Tait <isaac@mountaintopcoding.com>
2024-02-04 12:57:53 -0500

    chore: fix mobile layout about page 🙋🏽‍♀️

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -5,10 +5,9 @@ export default function About() {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <h1 className='flex flex-1 justify-center items-center'>
-        This is the&nbsp;
-        <span className='font-semibold'>About Page&nbsp;</span>
-        for the Connecticut Chapter of Moms Demand Action.
+      <h1 className='flex flex-1 justify-center items-center text-center'>
+        This is the About Page for the Connecticut Chapter of Moms
+        Demand Action.
       </h1>
       <div className='mt-auto'>
         <Footer />
