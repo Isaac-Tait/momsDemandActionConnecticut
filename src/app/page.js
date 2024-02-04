01e681259ba887565314d6c5ee846fbd411a2fae
Isaac Tait <isaac@mountaintopcoding.com>
2024-02-04 14:45:12 -0500

    chore: lots of data entry 😥

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Head from 'next/head';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -6,6 +6,10 @@ import Footer from '../components/Footer';
 export default function Home() {
   return (
     <div className='flex flex-col min-h-screen'>
+      <Head>
+        <title>Moms Demand Action Connecticut Chapter</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Header />
       <div className='mt-auto'>
         <Footer />
