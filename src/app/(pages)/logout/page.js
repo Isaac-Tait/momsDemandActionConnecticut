e41d420898d0e81ce40346aec9e14d8cd62ebbe6
Isaac Tait <isaac@mountaintopcoding.com>
2024-02-05 11:36:08 -0500

    feat: password protect senator page 👩🏾‍⚖️

--- src/app/(pages)/logout/page.js
+++ src/app/(pages)/logout/page.js
@@ -1,7 +1,19 @@
+import Header from '../../../components/Header';
+import Footer from '../../../components/Footer';
+
 export default function LogOut() {
   return (
-    <div>
-      <h1>Logout Page</h1>
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <h1 className='font-semibold text-2xl text-blue-500'>
+        Logout Page
+      </h1>
+      <p className='flex justify-center items-center text-xl font-medium'>
+        You have been logged out.
+      </p>
+      <div className='mt-auto'>
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/app/(pages)/senators/page.js
+++ src/app/(pages)/senators/page.js
@@ -1,37 +1,47 @@
 import Header from '../../../components/Header';
 import Footer from '../../../components/Footer';
 
+import {
+  withPageAuthRequired,
+  getSession,
+} from '@auth0/nextjs-auth0';
+
 import senatorList from '../../../util/senator.js';
 import SenatorCard from '../../../components/SenatorCard.js';
 
-export default function Senator() {
-  return (
-    <>
-      <Header />
-      <h1 className='font-cursive text-2xl pb-6 text-center text-red-500'>
-        Senators for{' '}
-        <span className='text-blue-500'>the great state</span> of
-        Connecticut!
-      </h1>
-      <div className='max-w-7xl mx-auto overflow-scroll-y z-10 pb-4 flex flex-wrap justify-center'>
-        {senatorList.map((d) => (
-          <SenatorCard
-            key={d.id}
-            firstName={d.firstName}
-            lastName={d.lastName}
-            district={d.district}
-            party={d.party}
-            yearElected={d.yearElected}
-            photoURL={d.photoURL}
-            gunSenseCandidate={d.gunSenseCandidate}
-            supportHB6667={d.supportHB6667}
-            senateDistrict={d.senateDistrict}
-          />
-        ))}
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </>
-  );
-}
+export default withPageAuthRequired(
+  async function Senators() {
+    const { user } = await getSession();
+    return (
+      <>
+        <Header />
+        <div>Hello {user.name}</div>
+        <h1 className='font-cursive text-2xl pb-6 text-center text-red-500'>
+          Senators for{' '}
+          <span className='text-blue-500'>the great state</span> of
+          Connecticut!
+        </h1>
+        <div className='max-w-7xl mx-auto overflow-scroll-y z-10 pb-4 flex flex-wrap justify-center'>
+          {senatorList.map((d) => (
+            <SenatorCard
+              key={d.id}
+              firstName={d.firstName}
+              lastName={d.lastName}
+              district={d.district}
+              party={d.party}
+              yearElected={d.yearElected}
+              photoURL={d.photoURL}
+              gunSenseCandidate={d.gunSenseCandidate}
+              supportHB6667={d.supportHB6667}
+              senateDistrict={d.senateDistrict}
+            />
+          ))}
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </>
+    );
+  },
+  { returnTo: '/senators' }
+);
