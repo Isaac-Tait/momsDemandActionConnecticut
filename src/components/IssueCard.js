710734367ed4cf984a73cf4c5e3aee5abf4cba74
Isaac Tait <isaac@mountaintopcoding.com>
2024-02-07 12:28:41 -0500

    chore: update link formatting 💡

--- src/components/IssueCard.js
+++ src/components/IssueCard.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export default function FormCard({ id, issueURL, issue, summary }) {
   return (
     <div
-      className='border-solid border-2 mx-2 bg-slate-100 p-1 rounded-lg flex flex-col mb-4 text-center'
+      className='border-solid border-2 mx-2 bg-slate-100 p-1 rounded-lg flex flex-col mb-4'
       key={id}
     >
       <Link href={issueURL} legacyBehavior passHref>
         <a
           target='_blank'
-          className='text-blue-500 underline p-1 hover:text-red-500 hover:bg-blue-300 hover:rounded-md text-sm md:text-lg'
+          className='text-blue-500 underline p-1 hover:text-red-500 hover:bg-blue-300 hover:rounded-md text-sm md:text-lg w-fit'
         >
           {issue}
         </a>
