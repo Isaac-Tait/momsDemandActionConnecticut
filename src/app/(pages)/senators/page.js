11fc931dcf0638af46155ec7c6327267c1499d9b
Isaac Tait <isaac@mountaintopcoding.com>
2024-02-23 10:45:19 -0500

    chore: remove auth0 page protections 🍆 🌮

--- src/app/(pages)/senators/page.js
+++ src/app/(pages)/senators/page.js
@@ -1,47 +1,51 @@
 import Header from '../../../components/Header';
 import Footer from '../../../components/Footer';
 
-import {
+/* import {
   withPageAuthRequired,
   getSession,
-} from '@auth0/nextjs-auth0';
+} from '@auth0/nextjs-auth0'; */
 
 import senatorList from '../../../util/senator.js';
 import SenatorCard from '../../../components/SenatorCard.js';
 
-export default withPageAuthRequired(
+/* export default withPageAuthRequired(
   async function Senators() {
-    const { user } = await getSession();
-    return (
-      <>
-        <Header />
-        <div>Hello {user.name}</div>
-        <h1 className='font-cursive text-2xl pb-6 text-center text-red-500'>
-          Senators for{' '}
-          <span className='text-blue-500'>the great state</span> of
-          Connecticut!
-        </h1>
-        <div className='max-w-7xl mx-auto overflow-scroll-y z-10 pb-4 flex flex-wrap justify-center'>
-          {senatorList.map((d) => (
-            <SenatorCard
-              key={d.id}
-              firstName={d.firstName}
-              lastName={d.lastName}
-              district={d.district}
-              party={d.party}
-              yearElected={d.yearElected}
-              photoURL={d.photoURL}
-              gunSenseCandidate={d.gunSenseCandidate}
-              supportHB6667={d.supportHB6667}
-              senateDistrict={d.senateDistrict}
-            />
-          ))}
-        </div>
-        <div>
-          <Footer />
-        </div>
-      </>
-    );
-  },
-  { returnTo: '/senators' }
-);
+    const { user } = await getSession(); */
+
+export default function SenatorsPage() {
+  return (
+    <>
+      <Header />
+      {/* <div>Hello {user.name}</div> */}
+      <h1 className='font-cursive text-2xl pb-6 text-center text-red-500'>
+        Senators for{' '}
+        <span className='text-blue-500'>the great state</span> of
+        Connecticut!
+      </h1>
+      <div className='max-w-7xl mx-auto overflow-scroll-y z-10 pb-4 flex flex-wrap justify-center'>
+        {senatorList.map((d) => (
+          <SenatorCard
+            key={d.id}
+            firstName={d.firstName}
+            lastName={d.lastName}
+            district={d.district}
+            party={d.party}
+            yearElected={d.yearElected}
+            photoURL={d.photoURL}
+            gunSenseCandidate={d.gunSenseCandidate}
+            supportHB6667={d.supportHB6667}
+            senateDistrict={d.senateDistrict}
+          />
+        ))}
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+/* },
+ { returnTo: '/senators' }
+); */
+
+}
